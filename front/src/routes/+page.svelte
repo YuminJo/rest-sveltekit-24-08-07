45b03e548ab102a5be26d0e12783894af834911f
조유민 <nationmilo@gmail.com>
2024-08-08 13:57:38 +0900

    feat: google font noto sans

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import '$lib/app.css';
+</script>
+
+<h1 class="text-red-500">Welcome to SvelteKit</h1>
+      
+<body>
+    <p>모든 인류 구성원의 천부의 존재 ㅁㄴㅇㅁㄴㅇ</p>
+</body>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
